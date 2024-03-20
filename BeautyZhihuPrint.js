752664ed8613bf70e4ee01714c96b2cf4cd1118a
Tajang <63721558+TajangSec@users.noreply.github.com>
2024-03-20 23:00:57 +0800

    更新版权
    更新版权

--- BeautyZhihuPrint.js
+++ BeautyZhihuPrint.js
@@ -7,6 +7,7 @@
 // @match        https://zhuanlan.zhihu.com/p/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zhihu.com
 // @grant        none
+// @license      GPL-3.0
 // ==/UserScript==
 
 window.onload = function () {
